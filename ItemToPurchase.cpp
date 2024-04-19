06e061e20428e8567992d6675ed2f8026b3be8d7
tlgls <167261062+tlgls@users.noreply.github.com>
2024-04-19 13:21:37 -0500

    Update ItemToPurchase.cpp
    Added setter methods.

--- ItemToPurchase.cpp
+++ ItemToPurchase.cpp
@@ -4,3 +4,14 @@ using namespace std;
 #include "ItemToPurchase.h"
 
 /* Type your code here */
+void::ItemToPurchase::SetName(string name) {
+  itemName = name;
+}
+
+void::ItemToPurchase::SetPrice(string price) {
+  itemPrice = price;
+}
+
+void::ItemToPurchase::SetQuantity(string qty) {
+  itemQuantity = qty;
+}
