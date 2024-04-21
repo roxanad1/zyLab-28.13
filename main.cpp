ba5a255e9cf6c539dc530ae582a3363abe4f7fd9
tlgls <167261062+tlgls@users.noreply.github.com>
2024-04-21 15:43:49 -0500

    Update main.cpp
    Fixed format.

--- main.cpp
+++ main.cpp
@@ -4,8 +4,54 @@ using namespace std;
 #include "ItemToPurchase.h"
 
 int main() {
+   ItemToPurchase itp;
+   ItemToPurchase itp2;
    
-   /* Type your code here */
+   string name;
+   int price;
+   int qty;
+
+   // item 1
+   cout << "Item 1" << endl;
+   cout << "Enter the item name:" << endl;
+   getline(cin, name);
+   itp.SetName(name);
+   
+   cout << "Enter the item price:" << endl;
+   cin >> price;
+   itp.SetPrice(price);
+   
+   cout << "Enter the item quantity:" << endl;
+   cin >> qty;
+   itp.SetQuantity(qty);
+   
+   cout << endl;
+
+   
+   // item 2
+   cout << "Item 2" << endl;
+   cout << "Enter the item name:" << endl;
+   cin.ignore();
+   getline(cin, name);
+   itp2.SetName(name);
+   
+   cout << "Enter the item price:" << endl;
+   cin >> price;
+   itp2.SetPrice(price);
+   
+   cout << "Enter the item quantity:" << endl;
+   cin >> qty;
+   itp2.SetQuantity(qty);
+   
+   cout << endl;
+
+   cout << "TOTAL COST" << endl;
+   cout << itp.GetName() << " " << itp.GetQuantity() << " @ $" << itp.GetPrice() << " = $" << itp.GetQuantity()*itp.GetPrice() << endl;
+   cout << itp2.GetName() << " " << itp2.GetQuantity() << " @ $" << itp2.GetPrice() << " = $" << itp2.GetQuantity()*itp2.GetPrice() << endl;
+
+   cout << endl;
+
+   cout << "Total: $" << (itp.GetQuantity()*itp.GetPrice())+(itp2.GetQuantity()*itp2.GetPrice()) << endl;
    
    return 0;
 }
